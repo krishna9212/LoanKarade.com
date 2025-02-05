df335088a5b869d97ecef37b4811793af7c0da2c
Krishna <134902959+krishna9212@users.noreply.github.com>
2025-02-05 20:59:54 +0530

    Update Landingpage.jsx

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -6,7 +6,7 @@ function Landingpage() {
   return (
     <div className=' h-full w-full '>
             <Navigation></Navigation>
-            <div className='md:h-[100%] pt-20 md:pt-0 w-[full] bg-gray-100 flex flex-col-reverse md:flex-row'>
+            <div className=' md:h-[100%] pt-20 md:pt-0 w-[full] bg-white flex flex-col-reverse md:flex-row'>
                 <div className="left -mt-[2.5rem] md:-mt-[0rem] md:h-full  md:w-[45%] flex flex-col px-4 p-0 md:p-10 md:pt-40  justify-center  text-black">
                     <h1 className='text-[3.7rem] md:text-[4.2rem] whitespace-nowrap poppins-semibold '>Need a Loan?</h1> 
                     <h1 className='md:text-[1.6rem] text-[1.4rem] whitespace-wrap poppins-light'>
