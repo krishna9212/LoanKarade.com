aab1d9a9b3ce7e207409e4fe99dc2b5438691edd
Krishna <134902959+krishna9212@users.noreply.github.com>
2025-02-10 20:52:37 +0530

    Redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navigation from "./Components/Navigation";
 import Cursor from "./Components/Cursor";
@@ -8,35 +8,34 @@ import LoanCategories from "./Components/LoanCatagories";
 import PersonalLoan from "./Components/PersonalLoan";
 import BusinessLoan from "./Components/BusinessLoan";
 import CreditCard from "./Components/CreditCard";
-import HomeLoan from "./Components/HomeLoan"
-import LoanAgainstProperty from "./Components/LoanAgainstProperty"
+import HomeLoan from "./Components/HomeLoan";
+import LoanAgainstProperty from "./Components/LoanAgainstProperty";
 import UsedCarLoan from "./Components/UsedCarLoan";
+import NotFound from "./Components/NotFound"; // Create this component for 404 handling
 
-// import UsedCarLoan from "./Components/UsedCarLoan"
 function App() {
   return (
-    // <testEmail/>
     <Router>
-       <div className="h-screen w-full bg-gray-100 dark:bg-gray-900">
-      <Navigation />
+      <div className="h-screen w-full bg-gray-100 dark:bg-gray-900">
+        <Navigation />
 
-         <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/loan-categories" element={<LoanCategories />} />
-           <Route path="/personal-loan" element={<PersonalLoan />} />
-           <Route path="/business-loan" element={<BusinessLoan />} />
-           <Route path="/credit-card" element={<CreditCard/>} />
-           <Route path="/home-loan" element={<HomeLoan/>} />
-           <Route path="/Loan-Against-Property" element={<LoanAgainstProperty/>} />
-           <Route path="/Used-Car-Loan" element={<UsedCarLoan/>} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/loan-categories" element={<LoanCategories />} />
+          <Route path="/personal-loan" element={<PersonalLoan />} />
+          <Route path="/business-loan" element={<BusinessLoan />} />
+          <Route path="/credit-card" element={<CreditCard />} />
+          <Route path="/home-loan" element={<HomeLoan />} />
+          <Route path="/loan-against-property" element={<LoanAgainstProperty />} />
+          <Route path="/used-car-loan" element={<UsedCarLoan />} />
+          <Route path="*" element={<NotFound />} /> {/* Handle unknown routes */}
+        </Routes>
 
-         </Routes>
-
-         <div className="cur hidden md:block">
-         <Cursor />
-         </div>
-       </div>
-     </Router>
+        <div className="cur hidden md:block">
+          <Cursor />
+        </div>
+      </div>
+    </Router>
   );
 }
 
